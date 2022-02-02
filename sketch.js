ae24cef94affafb4edda787f861d83460a136cdf
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2022-02-02 10:52:17 -0300

    Incorpora código base na interface

--- sketch.js
+++ sketch.js
@@ -4,31 +4,51 @@ var btn_left;
 var btn_right;
 var btn_front;
 var name_btn = ['CIMA', 'BAIXO', 'ESQUERDA', 'DIREITA', 'FRENTE'];
-var video;
 var cor = '#b52d24';
 var status_txt = "Bloqueado";
 var ok_brn;
 var estado = 0;
 
+let video;
+let videoSize = 64;
+let ready = false;
+
+let pixelBrain;
+let label = '';
+
 function setup() {
     // Cria canvas
-    var cnv = createCanvas(300, 260);
+    var cnv = createCanvas(300, 300);
     cnv.style('margin-left', '50%');
     cnv.style('margin-top', '5%');
     // Cria botões
-    criaBotao(btn_up, 0);
-    criaBotao(btn_down, 1);
-    criaBotao(btn_left, 2);
-    criaBotao(btn_right, 3);
-    criaBotao(btn_front, 4);
+    btn_up = criaBotao(btn_up, 0);
+    btn_down = criaBotao(btn_down, 1);
+    btn_left = criaBotao(btn_left, 2);
+    btn_right = criaBotao(btn_right, 3);
+    btn_front = criaBotao(btn_front, 4);
 
     ok_btn = select('#ok_btn');
     ok_btn.mousePressed(confirmaBtn);
+    btn_up.mousePressed(addExample);
+    btn_down.mousePressed(addExample);
+    btn_left.mousePressed(addExample);
+    btn_right.mousePressed(addExample);
+    btn_front.mousePressed(addExample);
+
 
     // Ativa vídeo
-    video = createCapture(VIDEO);
-    video.size(300,280);
+    video = createCapture(VIDEO, () => {ready = true});
+    video.size(videoSize, videoSize);
     video.hide();
+
+    // ML5
+    let options = {
+      inputs: [64, 64, 4],
+      task: 'imageClassification',
+      debug: true,
+    };
+    pixelBrain = ml5.neuralNetwork(options);
 
     // Pinta background
     background(cor);
@@ -39,11 +59,22 @@ function draw() {
     image(video, 0, 0);
     textSize(20);
     text(status_txt, 95, height - 10);
+     
+    if (ready) {
+      image(video, 0, 0, 300,270);
+    }
+
+    textSize(20);
+    textAlign(LEFT, LEFT);
+    fill("green");
+    text(label, 5, 20);
+    fill(255);
 }
 
 function criaBotao(var_btn, dir){
     var_btn = createButton(name_btn[dir]);
     var_btn.id("botao");
+    return var_btn;
 }
 
 function confirmaBtn(){
@@ -55,4 +86,57 @@ function confirmaBtn(){
         status_txt = 'Bloqueado';
     }
     estado = 1 - estado;
+
+    treinaModelo();
 }
+
+function addExample() {
+  var label = this.elt.innerText;
+  let inputImage = {
+    image: video,
+  };
+  let target = {
+    label,
+  };
+  console.log('Novo exemplo: ' + label);
+  pixelBrain.addData(inputImage, target);
+}
+
+function loaded() {
+  pixelBrain.train(
+    {
+      epochs: 10,
+    },
+    finishedTraining
+  );
+}
+function treinaModelo(){
+    pixelBrain.normalizeData();
+    pixelBrain.train(
+      {
+        epochs: 10,
+      },
+      finishedTraining
+    );
+}
+
+function finishedTraining() {
+  console.log('training complete');
+  classifyVideo();
+}
+
+function classifyVideo() {
+  let inputImage = {
+    image: video,
+  };
+  pixelBrain.classify(inputImage, gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    return;
+  }
+  label = results[0].label;
+  classifyVideo();
+}
+
